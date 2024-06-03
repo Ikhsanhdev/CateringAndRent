195493969bcf0a5b2132ee8256e7dcb1a1739ee4
IkhsanHadi <ikhsanhdn747@gmail.com>
2024-06-03 11:10:35 +0700

    init

---
+++ Controllers/RentController.cs
@@ -0,0 +1,20 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using cateringAndRent.Models;
+
+namespace cateringAndRent.Controllers;
+
+public class RentController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public RentController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace cateringAndRent.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
